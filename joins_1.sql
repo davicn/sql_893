1cad24cc8b1d5d044ae644369b46339535eb9427
Davi Nascimento <davi.nascimento777@gmail.com>
2022-12-23 21:29:48 -0300

    Update joins_1.sql

--- joins_1.sql
+++ joins_1.sql
@@ -58,6 +58,87 @@ inner join lanchonete.cliente c on c.id=p.id_cliente
 where ip.id_produto = 1
 group by c.nome;
 
--- select * from lanchonete.tipo_produto tp 
+-- select * from lanchonete.tipo_produto tp;
 
+create schema escola;
+
+
+create table escola.professor(
+id serial,
+nome varchar(20),
+primary key (id)
+);
+
+
+insert into escola.professor (nome)
+values 
+('Raimundo'),('Cleonice'),('Davi'),('Jorge');
+
+select * from escola.professor;
+
+
+create table escola.turma( 
+id serial,
+descricao varchar(30),
+id_professor integer,
+primary key(id)
+);
+
+insert into escola.turma(descricao, id_professor)
+values 
+('datilografia',1),
+('Costura',2),
+('Volei',9),
+('Peteca',7);
+
+select * from escola.turma;
+
+
+
+select * from escola.professor as p
+inner join  escola.turma as t on t.id_professor= p.id;
+
+-- Left Join
+
+select *
+from escola.professor as p
+left join  escola.turma as t on t.id_professor= p.id;
+
+-- Right Join
+select * from escola.professor as p
+right outer join  escola.turma as t on t.id_professor= p.id;
+
+-- Full join
+
+select * from escola.professor as p
+full join escola.turma as t on t.id_professor= p.id;
+
+
+
+-- Left Join
+select * from escola.professor as p
+right outer join  escola.turma as t on t.id_professor= p.id
+union 
+select *
+from escola.professor as p
+left join  escola.turma as t on t.id_professor= p.id
+
+
+create table teste (
+	id serial,
+	id_tipo integer not null,
+	primary key(id),
+	foreign key (id_tipo) references tipo_produto (id)
+)
+
+
+select * from tipo_produto 
+left join teste on tipo_produto.id = teste.id_tipo;
+
+select * from escola.turma as t
+right join  escola.professor as p on t.id_professor= p.id;
+
+
+select * from escola.professor
+where id not in (1,2,9,7);
 
